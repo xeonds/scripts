cc3975eaeb80cbc0573944d8023a577845af51af
xeonds <1456506329@qq.com>
2023-07-19 18:20:56 +0800

    Change output style to markdown

--- itnews.sh
+++ itnews.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -qO- https://www.ithome.com/block/rank.html | grep -oP '<li>\s*<a title="\K[^"]+'
+echo -e "# IT news Today\n\n$(wget -qO- https://www.ithome.com/block/rank.html | grep -oP '<li>\s*<a\K[^>]+' | awk -F '"' '{print "["$2"]("$6")"}' | sed 's/^/- /g')"
